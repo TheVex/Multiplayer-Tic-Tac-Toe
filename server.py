8dbb689d5a9acc3bf54c61a85de5280831d03fdf
Tovarish_Dru <Tovarish_Dru@DESKTOP-0IV7E7R>
2025-04-30 23:47:29 +0300

    Sever fixed

--- server.py
+++ server.py
@@ -226,6 +226,7 @@ class GameServer:
                             }
                             client.host_sock.send(json.dumps(win_resp).encode("utf-8"))
                         self.remove_game(game_id)
+                        break
                     if winner == Mark.DRAW:
                         game.finished = True
                         log.log_info(f"Game {game_id} finished. No winner")
@@ -237,6 +238,7 @@ class GameServer:
                             }
                             client.host_sock.send(json.dumps(win_resp).encode("utf-8"))
                         self.remove_game(game_id)
+                        break
                     game.change_turn()
                     for client in game.clients:
                         move_resp = {
@@ -268,6 +270,7 @@ class GameServer:
                                 except Exception as e:
                                     log.log_warn(f"Failed to notify player {game.clients[other_index].host_id} about opponent disconnection: {e}")
                             break
+                break
 
             except Exception as e:
                 log.log_error(f"Error handling client {client_addr}: {e}")
